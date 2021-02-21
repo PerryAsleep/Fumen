b7661cf1ded9fd31de636bfa87373b58aa55b602
Perry Lynch <perryklynch@gmail.com>
2021-02-21 11:07:12 -0500

    Fumen.Path commenting / cleanup.

--- Fumen/Path.cs
+++ Fumen/Path.cs
@@ -1,10 +1,26 @@
 ﻿
 namespace Fumen
 {
+	/// <summary>
+	/// Path static utility methods.
+	/// </summary>
 	public static class Path
 	{
+		/// <summary>
+		/// Combines two paths and returns the result.
+		/// Unlike System.IO.Path.Combine, this does not ignore the first path component
+		/// if the second path component begins with the directory separator character.
+		/// If the second path component is rooted with a drive root then the first path
+		/// component will be ignored.
+		/// </summary>
+		/// <param name="path1">First path component.</param>
+		/// <param name="path2">Second path component.</param>
+		/// <returns>Combined path.</returns>
 		public static string Combine(string path1, string path2)
 		{
+			// System.IO.Path.Combine will throw out the first path if the second path
+			// appears rooted. It treats a path which starts with the directory separator as rooted.
+			// Clean the second path to work around this behavior, and then combine.
 			if (System.IO.Path.IsPathRooted(path2))
 			{
 				path2 = path2.TrimStart(System.IO.Path.DirectorySeparatorChar);
@@ -13,6 +29,15 @@ namespace Fumen
 			return System.IO.Path.Combine(path1, path2);
 		}
 
+		/// <summary>
+		/// Combines the given paths and returns the result.
+		/// Unlike System.IO.Path.Combine, this does not ignore path components which
+		/// precede path components that begin with the directory separator character.
+		/// If a path component is rooted with a drive root then preceding path components
+		/// will be ignored.
+		/// </summary>
+		/// <param name="paths">Path components.</param>
+		/// <returns>Combined path.</returns>
 		public static string Combine(string[] paths)
 		{
 			if (paths == null || paths.Length == 0)
@@ -22,7 +47,11 @@ namespace Fumen
 			for (var i = 0; i < paths.Length; i++)
 			{
 				cleanedPaths[i] = paths[i];
-				if (System.IO.Path.IsPathRooted(cleanedPaths[i]))
+
+				// System.IO.Path.Combine will throw out the paths preceding a path that
+				// appears rooted. It treats a path which starts with the directory separator as rooted.
+				// Clean the paths to work around this behavior, and then combine.
+				if (i > 0 && System.IO.Path.IsPathRooted(cleanedPaths[i]))
 				{
 					cleanedPaths[i] = cleanedPaths[i].TrimStart(System.IO.Path.DirectorySeparatorChar);
 					cleanedPaths[i] = cleanedPaths[i].TrimStart(System.IO.Path.AltDirectorySeparatorChar);
