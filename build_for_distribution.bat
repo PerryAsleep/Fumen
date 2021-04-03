045212be340d439d368b741b85a2ba868a8d8bb6
Perry Lynch <perryklynch@gmail.com>
2021-04-03 09:09:53 -0400

    Made ship config file. Updated build script.

--- build_for_distribution.bat
+++ build_for_distribution.bat
@@ -1,13 +1,20 @@
+@REM Tools. Hard-coded and not included in repo. Brittle.
 set DEVENV=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe
 set ZIP=C:\Program Files\WinRAR\WinRAR.exe
 
+@REM Clean and build the solution.
 "%DEVENV%" Fumen.sln /Clean
 "%DEVENV%" Fumen.sln /Build Release
 
+@REM Remove any existing package.
 if exist Releases\StepManiaChartGenerator.zip (
     del Releases\StepManiaChartGenerator.zip
 )
 
+@REM Copy the config for shipping into the bin directory before packaging.
+xcopy .\StepManiaChartGenerator\config-ship.json .\StepManiaChartGenerator\bin\Release\config.json /Y
+
+@REM Zip the contents of the bin directory into a new package.
 chdir StepManiaChartGenerator\bin\Release
 "%ZIP%" a ..\..\..\Releases\StepManiaChartGenerator.zip *
 chdir ..\..\..
