ccc0961dfd83d70df585f1e5b17b2b7a2d65b54e
Perry Lynch <perryklynch@gmail.com>
2022-08-29 18:15:14 -0400

    Added float Lerp and Clamp methods.

--- Fumen/Interpolation.cs
+++ Fumen/Interpolation.cs
@@ -21,5 +21,21 @@ namespace Fumen
 		{
 			return value < min ? min : value > max ? max : value;
 		}
+
+		public static float Lerp(float startValue, float endValue, float startTime, float endTime, float currentTime)
+		{
+			if (endTime == startTime)
+				return currentTime >= endTime ? endValue : startValue;
+			var ret = startValue + ((currentTime - startTime) / (endTime - startTime)) * (endValue - startValue);
+			if (startValue < endValue)
+				return Clamp(ret, startValue, endValue);
+			return Clamp(ret, endValue, startValue);
+		}
+
+		// TODO: Move
+		public static float Clamp(float value, float min, float max)
+		{
+			return value < min ? min : value > max ? max : value;
+		}
 	}
 }
