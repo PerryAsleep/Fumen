PerryAsleep/Fumen

commit 09c8d7dfd836de4163882865a2a8b01c5d932266

Fixed scroll bugs with warps.

StepManiaEditor/EditorSong.cs
@@ -690,7 +690,7 @@ namespace StepManiaEditor
 				var chartEvent = rae.GetEvent();
 				if (chartEvent is Tempo tc)
 				{
-					var rowsSincePrevious = chartEvent.IntegerPosition - previousEvent.Row;
+					var rowsSincePrevious = Math.Max(0, chartEvent.IntegerPosition - previousEvent.RowForFollowingEvents);
 					var secondsSincePrevious = rowsSincePrevious * previousEvent.SecondsPerRow;
 
 					var newSecondsPerRow = 60.0 / tc.TempoBPM / (double)SMCommon.MaxValidDenominator;
@@ -735,7 +735,7 @@ namespace StepManiaEditor
 				}
 				else if (chartEvent is Stop stop)
 				{
-					var rowsSincePrevious = chartEvent.IntegerPosition - previousEvent.Row;
+					var rowsSincePrevious = Math.Max(0, chartEvent.IntegerPosition - previousEvent.RowForFollowingEvents);
 					var secondsSincePrevious = rowsSincePrevious * previousEvent.SecondsPerRow;
 
 					rae.Row = chartEvent.IntegerPosition;
@@ -754,7 +754,7 @@ namespace StepManiaEditor
 				}
 				else if (chartEvent is Warp warp)
 				{
-					var rowsSincePrevious = chartEvent.IntegerPosition - previousEvent.Row;
+					var rowsSincePrevious = Math.Max(0, chartEvent.IntegerPosition - previousEvent.RowForFollowingEvents);
 					var secondsSincePrevious = rowsSincePrevious * previousEvent.SecondsPerRow;
 
 					rae.Row = chartEvent.IntegerPosition;
@@ -772,7 +772,7 @@ namespace StepManiaEditor
 				}
 				else if (chartEvent is ScrollRate scrollRate)
 				{
-					var rowsSincePrevious = chartEvent.IntegerPosition - previousEvent.Row;
+					var rowsSincePrevious = Math.Max(0, chartEvent.IntegerPosition - previousEvent.RowForFollowingEvents);
 					var secondsSincePrevious = rowsSincePrevious * previousEvent.SecondsPerRow;
 
 					// Update any events which precede the first tempo so they can have accurate rates.
@@ -802,7 +802,7 @@ namespace StepManiaEditor
 				}
 				else if (chartEvent is TimeSignature timeSignature)
 				{
-					var rowsSincePrevious = chartEvent.IntegerPosition - previousEvent.Row;
+					var rowsSincePrevious = Math.Max(0, chartEvent.IntegerPosition - previousEvent.RowForFollowingEvents);
 					var secondsSincePrevious = rowsSincePrevious * previousEvent.SecondsPerRow;
 
 					rae.Row = chartEvent.IntegerPosition;
@@ -855,11 +855,8 @@ namespace StepManiaEditor
 			var rateEvent = GetActiveRateAlteringEventForTime(chartTime, false);
 			if (rateEvent == null)
 				return false;
-			
-			if (chartTime >= rateEvent.SongTime && chartTime < rateEvent.SongTimeForFollowingEvents)
-				chartPosition = rateEvent.Row;
-			else
-				chartPosition = rateEvent.Row + rateEvent.RowsPerSecond * (chartTime - rateEvent.SongTimeForFollowingEvents);
+
+			chartPosition = rateEvent.RowForFollowingEvents + rateEvent.RowsPerSecond * Math.Max(0.0, chartTime - rateEvent.SongTimeForFollowingEvents);
 			return true;
 		}
 
@@ -889,7 +886,8 @@ namespace StepManiaEditor
 			var rateEvent = GetActiveRateAlteringEventForPosition(chartPosition, false);
 			if (rateEvent == null)
 				return false;
-			chartTime = rateEvent.SongTimeForFollowingEvents + rateEvent.SecondsPerRow * (chartPosition - rateEvent.Row);
+
+			chartTime = rateEvent.SongTimeForFollowingEvents + rateEvent.SecondsPerRow * Math.Max(0.0, chartPosition - rateEvent.RowForFollowingEvents);
 			return true;
 		}
 

commit 28ad2594316a0ac1a504e8affbaed73b3573ada0

Fixed regression from 09c8d7dfd836de4163882865a2a8b01c5d932266 where the position and time were not being set correctly before 0.0.

StepManiaEditor/Editor.cs
@@ -1306,6 +1306,19 @@ namespace StepManiaEditor
 						else
 							interpolatedScrollRate = interpolatedRateEvent.GetInterpolatedScrollRateFromRow(chartPosition);
 					}
+					else
+					{
+						interpolatedScrollRateEnumerator = ActiveChart.InterpolatedScrollRateEvents.FindLeastFollowing(ratePosEventForChecking, true);
+						if (interpolatedScrollRateEnumerator != null)
+						{
+							interpolatedScrollRateEnumerator.MoveNext();
+							var interpolatedRateEvent = interpolatedScrollRateEnumerator.Current;
+							if (interpolatedRateEvent.InterpolatesByTime())
+								interpolatedScrollRate = interpolatedRateEvent.GetInterpolatedScrollRateFromTime(time);
+							else
+								interpolatedScrollRate = interpolatedRateEvent.GetInterpolatedScrollRateFromRow(chartPosition);
+						}
+					}
 
 					// Now, scroll up to the top of the screen so we can start processing events going downwards.
 					// We know what time / pos we are drawing at the receptors, but not the rate to get to that time from the top
@@ -2612,11 +2625,11 @@ namespace StepManiaEditor
 						p.RecentFiles.Count - pOptions.RecentFilesHistorySize);
 				}
 
-				// Find a better spot for this
+				OnChartSelected(newActiveChart, false);
+
+				// Find a better spot for this.
 				Position.ChartPosition = 0.0;
 				DesiredSongTime = Position.SongTime;
-
-				OnChartSelected(newActiveChart, false);
 
 				SetZoom(1.0, true);
 			}

StepManiaEditor/EditorSong.cs
@@ -834,7 +834,10 @@ namespace StepManiaEditor
 					{
 						irae.Row = scrollRateInterpolation.IntegerPosition;
 						irae.SongTime = scrollRateInterpolation.TimeMicros;
-						irae.PreviousScrollRate = lastScrollRateInterpolationValue;
+						// For the first scroll rate event, set the previous rate to the first rate so we use the
+						// first scroll rate when consider positions and times before 0.0. See also
+						// OnInterpolatedRateAlteringEventModified.
+						irae.PreviousScrollRate = firstInterpolatedScrollRate ? scrollRateInterpolation.Rate : lastScrollRateInterpolationValue;
 						irae.CanBeDeleted = !firstInterpolatedScrollRate;
 						interpolatedScrollRateEvents.Insert(irae);
 						lastScrollRateInterpolationValue = scrollRateInterpolation.Rate;
@@ -1056,7 +1059,12 @@ namespace StepManiaEditor
 			if (rateEvent == null)
 				return false;
 
-			chartPosition = rateEvent.RowForFollowingEvents + rateEvent.RowsPerSecond * Math.Max(0.0, chartTime - rateEvent.SongTimeForFollowingEvents);
+			// Cap the relative time to 0.0 so warps function properly, except when the chart time is
+			// before 0.0.
+			var relativeTime = chartTime - rateEvent.SongTimeForFollowingEvents;
+			if (chartTime > 0.0 && relativeTime < 0.0)
+				relativeTime = 0.0;
+			chartPosition = rateEvent.RowForFollowingEvents + rateEvent.RowsPerSecond * relativeTime;
 			return true;
 		}
 
@@ -1087,7 +1095,12 @@ namespace StepManiaEditor
 			if (rateEvent == null)
 				return false;
 
-			chartTime = rateEvent.SongTimeForFollowingEvents + rateEvent.SecondsPerRow * Math.Max(0.0, chartPosition - rateEvent.RowForFollowingEvents);
+			// Cap the relative position to 0.0 so warps function properly, except when the chart time is
+			// before 0.0.
+			var relativePosition = chartPosition - rateEvent.RowForFollowingEvents;
+			if (chartPosition > 0.0 && relativePosition < 0)
+				relativePosition = 0.0;
+			chartTime = rateEvent.SongTimeForFollowingEvents + rateEvent.SecondsPerRow * relativePosition;
 			return true;
 		}
 
@@ -1255,6 +1268,17 @@ namespace StepManiaEditor
 			if (e != null)
 			{
 				e.MoveNext();
+
+				// If this is the first event, set its PreviousScrollRate as well so when we consider times
+				// and positions before 0.0 we use the first scroll rate.
+				// See also SetUpEditorEvents.
+				var first = !e.MovePrev();
+				e.MoveNext();
+				if (first)
+				{
+					e.Current.PreviousScrollRate = irae.ScrollRateInterpolationEvent.Rate;
+				}
+
 				if (e.MoveNext())
 				{
 					var next = e.Current;