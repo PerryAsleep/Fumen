269d66f006f11ae6260d6357222edd62c8d9b40c
Perry Lynch <perryklynch@gmail.com>
2023-05-06 15:36:43 -0400

    StepManiaChartGenerator uses semantic versioning. Updated to v2.0.0

---
+++ StepManiaChartGeneratorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StepManiaChartGeneratorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StepManiaChartGeneratorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c432230-aa56-4d1f-9d19-5a17551465a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
