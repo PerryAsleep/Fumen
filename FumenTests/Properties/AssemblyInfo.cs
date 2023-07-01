15706704ed52c9d14ba32821edea167101a71c6a
Perry Lynch <perryklynch@gmail.com>
2023-07-01 19:16:08 -0400

    Update to .Net 7.0

--- FumenTests/Properties/AssemblyInfo.cs
+++ FumenTests/Properties/AssemblyInfo.cs
@@ -1,19 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FumenTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FumenTests")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("6129c91b-f26b-423f-97ec-b4d4a7ecb631")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
