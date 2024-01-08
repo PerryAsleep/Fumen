e94c3e1775d17c07de79e3a8e222200bfe1e6cbc
Perry Lynch <perryklynch@gmail.com>
2024-01-08 18:42:26 -0500

    Add hermite interpolation and lerp with int times.

--- Fumen/Interpolation.cs
+++ Fumen/Interpolation.cs
@@ -35,6 +35,21 @@ public class Interpolation
 	}
 
 	/// <summary>
+	/// Return a linearly interpolated value between the given start and end values based on the given
+	/// current time with respect to the given start and end times.
+	/// The returned value will be clamped to be within the range defined by the given start and end values.
+	/// </summary>
+	public static float Lerp(float startValue, float endValue, int startTime, int endTime, int currentTime)
+	{
+		if (endTime == startTime)
+			return currentTime >= endTime ? endValue : startValue;
+		var ret = startValue + (float)(currentTime - startTime) / (endTime - startTime) * (endValue - startValue);
+		if (startValue < endValue)
+			return MathUtils.Clamp(ret, startValue, endValue);
+		return MathUtils.Clamp(ret, endValue, startValue);
+	}
+
+	/// <summary>
 	/// Return a logarithmicly interpolated value between the given start and end values based on the given
 	/// current time with respect to the given start and end times.
 	/// The returned value will be clamped to be within the range defined by the given start and end values.
@@ -56,5 +71,24 @@ public class Interpolation
 		double currentTime)
 	{
 		return Lerp(startValue, endValue, Math.Log(startTime), Math.Log(endTime), Math.Log(currentTime));
+	}
+
+	/// <summary>
+	/// Performs Hermite spline interpolation of values for four sequential, equally-spaced points.
+	/// Adapted from https://stackoverflow.com/a/72122178
+	/// </summary>
+	/// <param name="x0">Point 0.</param>
+	/// <param name="x1">Point 1.</param>
+	/// <param name="x2">Point 2.</param>
+	/// <param name="x3">Point 3.</param>
+	/// <param name="t">Time value, normalized such that t at x1 is 0.0f and t at x2 is 1.0f.</param>
+	/// <returns>Interpolated result at time t.</returns>
+	public static float HermiteInterpolate(float x0, float x1, float x2, float x3, float t)
+	{
+		var d = x1 - x2;
+		var c1 = x2 - x0;
+		var c3 = x3 - x0 + 3.0f * d;
+		var c2 = -2.0f * d - c1 - c3;
+		return 0.5f * ((c3 * t + c2) * t + c1) * t + x1;
 	}
 }
