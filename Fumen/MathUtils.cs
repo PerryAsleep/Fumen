d8adf02fca58a26eec002c9af1fdd9de3b367c03
Perry Lynch <perryklynch@gmail.com>
2024-08-14 19:21:21 -0400

    Add GetNextPowerOfTwo function.

--- Fumen/MathUtils.cs
+++ Fumen/MathUtils.cs
@@ -25,4 +25,16 @@ public class MathUtils
 			return int.MaxValue;
 		return (int)value;
 	}
+
+	public static int GetNextPowerOfTwo(int value)
+	{
+		value--;
+		value |= value >> 1;
+		value |= value >> 2;
+		value |= value >> 4;
+		value |= value >> 8;
+		value |= value >> 16;
+		value++;
+		return value;
+	}
 }
