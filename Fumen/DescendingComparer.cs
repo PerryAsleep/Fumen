37f55801e7a4eeb102371df2cfe9c75e2ca206fd
Perry Lynch <perryklynch@gmail.com>
2024-08-14 19:20:54 -0400

    Add DescendingComparer class.

---
+++ Fumen/DescendingComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Fumen;
+
+public sealed class DescendingComparer<T> : IComparer<T>
+{
+	public int Compare(T first, T second)
+	{
+		if (first == null)
+			return -1;
+		if (second == null)
+			return 1;
+		return Comparer<T>.Default.Compare(second, first);
+	}
+}
