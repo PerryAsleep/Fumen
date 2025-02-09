b5b2c228932a4774f1e7cab3afa57a74e1d02264
Perry Lynch <perryklynch@gmail.com>
2025-02-09 09:15:29 -0500

    Add printf helpers to mitigate need for windows API calls.

---
+++ Fumen/Printf.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Text;
+
+namespace Fumen;
+
+/// <summary>
+/// Class for offering C++ printf functionality.
+/// </summary>
+public static class Printf
+{
+	private class FormatSpecifier
+	{
+		public string Flags { get; set; } = "";
+		public int? Width { get; set; }
+		public int? Precision { get; set; }
+		public string Length { get; set; } = "";
+		public char Specifier { get; set; }
+	}
+
+	private static bool IsFlag(char c)
+	{
+		return "+-#0 ".Contains(c);
+	}
+
+	private static bool IsLength(char c)
+	{
+		// ReSharper disable once StringLiteralTypo
+		return "hlLzjt".Contains(c);
+	}
+
+	private static bool IsNumeric(object obj)
+	{
+		return obj is sbyte or byte or short or ushort or int or uint or long or ulong or float or double or decimal;
+	}
+
+	public static string Sprintf(string format, params object[] args)
+	{
+		var sb = new StringBuilder();
+		var argIndex = 0;
+
+		for (var i = 0; i < format?.Length; i++)
+		{
+			if (format[i] != '%')
+			{
+				sb.Append(format[i]);
+				continue;
+			}
+
+			if (i + 1 >= format.Length)
+				break;
+
+			var spec = ParseFormatSpecifier(format, ref i);
+			if (spec.Specifier == '%')
+			{
+				sb.Append('%');
+				continue;
+			}
+
+			if (args != null)
+			{
+				if (argIndex >= args.Length)
+					break;
+				sb.Append(FormatArgumentToString(args[argIndex++], spec));
+			}
+		}
+
+		return sb.ToString();
+	}
+
+	private static FormatSpecifier ParseFormatSpecifier(string format, ref int i)
+	{
+		var spec = new FormatSpecifier();
+
+		// Skip %.
+		i++;
+
+		while (i < format.Length && IsFlag(format[i]))
+			spec.Flags += format[i++];
+
+		var widthStr = "";
+		while (i < format.Length && char.IsDigit(format[i]))
+			widthStr += format[i++];
+		if (!string.IsNullOrEmpty(widthStr))
+			spec.Width = int.Parse(widthStr);
+
+		if (i < format.Length && format[i] == '.')
+		{
+			i++;
+			var precisionStr = "";
+			while (i < format.Length && char.IsDigit(format[i]))
+				precisionStr += format[i++];
+			if (!string.IsNullOrEmpty(precisionStr))
+				spec.Precision = int.Parse(precisionStr);
+		}
+
+		while (i < format.Length && IsLength(format[i]))
+			spec.Length += format[i++];
+
+		if (i < format.Length)
+			spec.Specifier = format[i];
+
+		return spec;
+	}
+
+	private static string FormatNumber(string numStr, FormatSpecifier spec, bool isNegative = false)
+	{
+		var sign = "";
+		if (isNegative)
+			sign = "-";
+		else if (spec.Flags.Contains('+'))
+			sign = "+";
+		else if (spec.Flags.Contains(' '))
+			sign = " ";
+
+		var effectiveWidth = spec.Width.HasValue ? spec.Width.Value - sign.Length : 0;
+
+		if (effectiveWidth > numStr.Length && !spec.Flags.Contains('-'))
+		{
+			var padChar = spec.Flags.Contains('0') ? '0' : ' ';
+			numStr = numStr.PadLeft(effectiveWidth, padChar);
+		}
+
+		var result = sign + numStr;
+
+		if (effectiveWidth > numStr.Length && spec.Flags.Contains('-'))
+			result = result.PadRight(spec.Width!.Value);
+
+		return result;
+	}
+
+	private static string FormatArgumentToString(object arg, FormatSpecifier spec)
+	{
+		switch (spec.Specifier)
+		{
+			case 'd':
+			case 'i':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToInt64(arg);
+					return FormatNumber(Math.Abs(number).ToString(), spec, number < 0);
+				}
+
+				break;
+			}
+			case 'u':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToUInt64(arg);
+					return FormatNumber(number.ToString(), spec);
+				}
+
+				break;
+			}
+			case 'f':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToDouble(arg);
+					var format = "F" + (spec.Precision ?? 6);
+					var numStr = Math.Abs(number).ToString(format);
+					return FormatNumber(numStr, spec, number < 0);
+				}
+
+				break;
+			}
+			case 'e':
+			case 'E':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToDouble(arg);
+					var format = spec.Specifier + (spec.Precision ?? 6).ToString();
+					var numStr = Math.Abs(number).ToString(format);
+					return FormatNumber(numStr, spec, number < 0);
+				}
+
+				break;
+			}
+			case 'g':
+			case 'G':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToDouble(arg);
+					var format = spec.Specifier + (spec.Precision ?? 6).ToString();
+					var numStr = Math.Abs(number).ToString(format);
+					return FormatNumber(numStr, spec, number < 0);
+				}
+
+				break;
+			}
+			case 'x':
+			case 'X':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToInt64(arg);
+					var format = spec.Specifier.ToString();
+					var numStr = Math.Abs(number).ToString(format);
+					if (spec.Flags.Contains('#') && number != 0)
+						numStr = "0" + spec.Specifier + numStr;
+					return FormatNumber(numStr, spec, number < 0);
+				}
+
+				break;
+			}
+
+			case 'o':
+			{
+				if (IsNumeric(arg))
+				{
+					var number = Convert.ToInt64(arg);
+					var numStr = Convert.ToString(Math.Abs(number), 8);
+					if (spec.Flags.Contains('#') && number != 0)
+						numStr = "0" + numStr;
+					return FormatNumber(numStr, spec, number < 0);
+				}
+
+				break;
+			}
+
+			case 's':
+			{
+				var str = arg?.ToString() ?? "";
+				if (spec.Precision.HasValue)
+					str = str.Substring(0, Math.Min(str.Length, spec.Precision.Value));
+				if (spec.Width.HasValue)
+				{
+					if (spec.Flags.Contains('-'))
+						str = str.PadRight(spec.Width.Value);
+					else
+						str = str.PadLeft(spec.Width.Value);
+				}
+
+				return str;
+			}
+
+			case 'c':
+			{
+				var c = Convert.ToChar(arg).ToString();
+				if (spec.Width.HasValue)
+				{
+					if (spec.Flags.Contains('-'))
+						c = c.PadRight(spec.Width.Value);
+					else
+						c = c.PadLeft(spec.Width.Value);
+				}
+
+				return c;
+			}
+		}
+
+		return "";
+	}
+}
---
+++ FumenTests/TestPrintf.cs
@@ -0,0 +1,123 @@
+﻿using Fumen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FumenTests;
+
+[TestClass]
+public class TestPrintf
+{
+	[TestMethod]
+	public void TestEdgeCases()
+	{
+		Assert.AreEqual("", Printf.Sprintf(null));
+		Assert.AreEqual("", Printf.Sprintf(""));
+		Assert.AreEqual("", Printf.Sprintf("%s", null));
+		Assert.AreEqual("", Printf.Sprintf("%s", ""));
+		Assert.AreEqual("", Printf.Sprintf("%s"));
+		Assert.AreEqual("test", Printf.Sprintf("test"));
+		Assert.AreEqual("test", Printf.Sprintf("test", "test-arg"));
+		Assert.AreEqual("", Printf.Sprintf("%.0s", "test"));
+	}
+
+	[TestMethod]
+	public void TestIntegerFormatting()
+	{
+		Assert.AreEqual("123", Printf.Sprintf("%d", 123));
+		Assert.AreEqual("123", Printf.Sprintf("%i", 123));
+		Assert.AreEqual("+123", Printf.Sprintf("%+d", 123));
+		Assert.AreEqual("-123", Printf.Sprintf("%d", -123));
+		Assert.AreEqual("00123", Printf.Sprintf("%05d", 123));
+		Assert.AreEqual("123  ", Printf.Sprintf("%-5d", 123));
+	}
+
+	[TestMethod]
+	public void TestUnsignedFormatting()
+	{
+		Assert.AreEqual("4294967295", Printf.Sprintf("%u", uint.MaxValue));
+		Assert.AreEqual("00123", Printf.Sprintf("%05u", 123));
+	}
+
+	[TestMethod]
+	public void TestFloatFormatting()
+	{
+		Assert.AreEqual("3.142", Printf.Sprintf("%.3f", 3.14159));
+		Assert.AreEqual("+3.14", Printf.Sprintf("%+.2f", 3.14159));
+		Assert.AreEqual("003.14", Printf.Sprintf("%06.2f", 3.14159));
+		Assert.AreEqual("3.14  ", Printf.Sprintf("%-6.2f", 3.14159));
+	}
+
+	[TestMethod]
+	public void TestScientificNotation()
+	{
+		Assert.AreEqual("1.234e+003", Printf.Sprintf("%.3e", 1234.0));
+		Assert.AreEqual("1.234E+003", Printf.Sprintf("%.3E", 1234.0));
+	}
+
+	[TestMethod]
+	public void TestGeneralFormat()
+	{
+		Assert.AreEqual("123.456", Printf.Sprintf("%g", 123.456));
+		Assert.AreEqual("1.23457e+08", Printf.Sprintf("%g", 123456789.0));
+		Assert.AreEqual("123.4568", Printf.Sprintf("%.7g", 123.456789));
+	}
+
+	[TestMethod]
+	public void TestHexadecimalFormatting()
+	{
+		Assert.AreEqual("ff", Printf.Sprintf("%x", 255));
+		Assert.AreEqual("FF", Printf.Sprintf("%X", 255));
+		Assert.AreEqual("00ff", Printf.Sprintf("%04x", 255));
+	}
+
+	[TestMethod]
+	public void TestOctalFormatting()
+	{
+		Assert.AreEqual("100", Printf.Sprintf("%o", 64));
+		Assert.AreEqual("0100", Printf.Sprintf("%#o", 64));
+	}
+
+	[TestMethod]
+	public void TestStringFormatting()
+	{
+		Assert.AreEqual("Test", Printf.Sprintf("%s", "Test"));
+		Assert.AreEqual("Tes", Printf.Sprintf("%.3s", "Test"));
+		Assert.AreEqual("Test      ", Printf.Sprintf("%-10s", "Test"));
+		Assert.AreEqual("      Test", Printf.Sprintf("%10s", "Test"));
+	}
+
+	[TestMethod]
+	public void TestCharacterFormatting()
+	{
+		Assert.AreEqual("A", Printf.Sprintf("%c", 'A'));
+		Assert.AreEqual("A    ", Printf.Sprintf("%-5c", 'A'));
+	}
+
+	[TestMethod]
+	public void TestMultipleFormatting()
+	{
+		Assert.AreEqual("123 Test 3.14", Printf.Sprintf("%d %s %.2f", 123, "Test", 3.14159));
+	}
+
+	[TestMethod]
+	public void TestEscapedPercent()
+	{
+		Assert.AreEqual("123%", Printf.Sprintf("%d%%", 123));
+		Assert.AreEqual("%", Printf.Sprintf("%%"));
+	}
+
+	[TestMethod]
+	public void TestPrecisionAndWidth()
+	{
+		Assert.AreEqual("  3.14", Printf.Sprintf("%6.2f", 3.14159));
+		Assert.AreEqual("003.14", Printf.Sprintf("%06.2f", 3.14159));
+		Assert.AreEqual("3.14  ", Printf.Sprintf("%-6.2f", 3.14159));
+	}
+
+	[TestMethod]
+	public void TestFlagCombinations()
+	{
+		Assert.AreEqual("+00123", Printf.Sprintf("%+06d", 123));
+		Assert.AreEqual("+123  ", Printf.Sprintf("%-+6d", 123));
+		Assert.AreEqual(" 123", Printf.Sprintf("% d", 123));
+	}
+}
