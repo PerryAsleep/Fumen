984b559c946957918005c73bb860b8afaec37952
Perry Lynch <perryklynch@gmail.com>
2025-03-04 20:53:01 -0500

    Add pinned buffer class for pinning managed buffers.

---
+++ Fumen/PinnedBuffer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Fumen;
+
+/// <summary>
+/// Class for wrapping a managed buffer with a pinned handle.
+/// </summary>
+public sealed class PinnedBuffer : IDisposable
+{
+	/// <summary>
+	/// Pinned handle to managed data.
+	/// </summary>
+	private GCHandle Handle;
+
+	/// <summary>
+	/// Managed data.
+	/// </summary>
+	private readonly byte[] Data;
+
+	public PinnedBuffer(byte[] data)
+	{
+		Data = data;
+		Handle = GCHandle.Alloc(Data, GCHandleType.Pinned);
+	}
+
+	~PinnedBuffer()
+	{
+		Dispose(false);
+	}
+
+	public byte[] GetPinnedData()
+	{
+		return Data;
+	}
+
+	public IntPtr GetPinnedDataHandle()
+	{
+		return Handle.AddrOfPinnedObject();
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	private void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			Handle.Free();
+		}
+	}
+}
