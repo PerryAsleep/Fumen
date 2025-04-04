4be690243da8dbba40c16573db5d01b705b94ac7
Perry Lynch <perryklynch@gmail.com>
2025-04-03 21:20:02 -0400

    Add ini file parsing and ITGmania pack file parsing.

---
+++ Fumen/Converters/IniFile.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using static Fumen.Logger;
+
+namespace Fumen.Converters;
+
+/// <summary>
+/// Ini file.
+/// There is no official spec for ini files. This class is based off of Stepmania ini parsing logic.
+/// </summary>
+public class IniFile
+{
+	/// <summary>
+	/// Individual Group within the file.
+	/// </summary>
+	public class Group
+	{
+		/// <summary>
+		/// Group name.
+		/// </summary>
+		public readonly string Name;
+
+		/// <summary>
+		/// All the values for this Group.
+		/// </summary>
+		public readonly Dictionary<string, string> Values = [];
+
+		public Group(string name)
+		{
+			Name = name;
+		}
+	}
+
+	/// <summary>
+	/// List of all Groups defined in the file.
+	/// </summary>
+	public List<Group> Groups = [];
+
+	/// <summary>
+	/// Asynchronously load the ini file at the specified path.
+	/// </summary>
+	/// <param name="filePath">Path to the ini file.</param>
+	/// <param name="token">CancellationToken to cancel task.</param>
+	/// <returns>Whether the file was loaded successfully or not.</returns>
+	public static async Task<IniFile> LoadAsync(string filePath, CancellationToken token)
+	{
+		var iniFile = new IniFile();
+
+		// Load the file into an array of lines to parse.
+		string[] lines;
+		try
+		{
+			lines = await File.ReadAllLinesAsync(filePath, token);
+		}
+		catch (OperationCanceledException)
+		{
+			throw;
+		}
+		catch (Exception e)
+		{
+			Error($"[INI] Failed to read {filePath}.");
+			Error($"[INI] {e}");
+			return null;
+		}
+
+		token.ThrowIfCancellationRequested();
+
+		// Parse the lines.
+		await Task.Run(() =>
+		{
+			void ParseValue(Group currentGroup, string val)
+			{
+				if (currentGroup == null)
+					return;
+				var kvp = val.Split('=', 2);
+				if (kvp != null && kvp.Length == 2)
+				{
+					var key = kvp[0].Trim();
+					if (!string.IsNullOrEmpty(key))
+					{
+						if (!currentGroup.Values.TryAdd(key, kvp[1]))
+						{
+							Warn($"[INI] [{currentGroup.Name}] has multiple entries for \"{key}\". Ignoring \"{kvp[1]}\"");
+						}
+					}
+				}
+				else
+				{
+					Warn($"[INI] Could not split line on '=': \"{val}\"");
+				}
+			}
+
+			Group currentGroup = null;
+
+			foreach (var line in lines)
+			{
+				if (string.IsNullOrEmpty(line))
+					continue;
+				switch (line[0])
+				{
+					case ';':
+					case '#':
+						// Comment
+						continue;
+					case '/':
+					case '-':
+						// "//" or "--" comment.
+						if (line.Length > 1 && line[0] == line[1])
+							continue;
+						ParseValue(currentGroup, line);
+						break;
+					case '[':
+						if (line[^1] == ']')
+						{
+							var groupName = line.Substring(1, line.Length - 2);
+							currentGroup = new Group(groupName);
+							iniFile.Groups.Add(currentGroup);
+							break;
+						}
+
+						ParseValue(currentGroup, line);
+						break;
+					default:
+						ParseValue(currentGroup, line);
+						break;
+				}
+			}
+		}, token);
+
+		return iniFile;
+	}
+
+	/// <summary>
+	/// Asynchronously save the IniFile to the given file path.
+	/// </summary>
+	/// <param name="filePath">Path to save to.</param>
+	/// <returns>True if saving was successful and false otherwise.</returns>
+	public async Task<bool> SaveAsync(string filePath)
+	{
+		return await Task.Run(() => Save(filePath));
+	}
+
+	/// <summary>
+	/// Synchronously save the IniFile to the given file path.
+	/// </summary>
+	/// <param name="filePath">Path to save to.</param>
+	/// <returns>True if saving was successful and false otherwise.</returns>
+	public bool Save(string filePath)
+	{
+		var previousCulture = CultureInfo.CurrentCulture;
+		try
+		{
+			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+			using var atomicFile = new AtomicPersistenceFile(filePath);
+			using var streamWriter = new StreamWriter(atomicFile.GetFilePathToSaveTo());
+			foreach (var group in Groups)
+			{
+				streamWriter.WriteLine($"[{group.Name}]");
+				foreach (var kvp in group.Values)
+				{
+					streamWriter.WriteLine($"{kvp.Key}={kvp.Value}");
+				}
+			}
+		}
+		finally
+		{
+			CultureInfo.CurrentCulture = previousCulture;
+		}
+
+		return true;
+	}
+}
---
+++ Fumen/Converters/ItgManiaPack.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fumen.Converters;
+
+/// <summary>
+/// ItgMania Pack file.
+/// </summary>
+public class ItgManiaPack : ICloneable
+{
+	public enum SyncOffSetType
+	{
+		// ReSharper disable InconsistentNaming
+		NULL,
+		ITG,
+		// ReSharper restore InconsistentNaming
+	}
+
+	private const int LatestVersion = 1;
+	public const string FileName = "Pack.ini";
+
+	private const string TagGroup = "Group";
+	private const string TagVersion = "Version";
+	private const string TagTitle = "DisplayTitle";
+	private const string TagTitleTransliteration = "TranslitTitle";
+	private const string TagTitleSort = "SortTitle";
+	private const string TagSeries = "Series";
+	private const string TagYear = "Year";
+	private const string TagBanner = "Banner";
+	private const string TagSyncOffset = "SyncOffset";
+
+	public string Title { get; set; }
+	public string TitleTransliteration { get; set; }
+	public string TitleSort { get; set; }
+	public string Series { get; set; }
+	public int Year { get; set; }
+	public string Banner { get; set; }
+	public SyncOffSetType SyncOffset { get; set; }
+
+	/// <summary>
+	/// Asynchronously load the pack file from disk.
+	/// </summary>
+	/// <param name="filePath">Path to the file to load.</param>
+	/// <param name="token">CancellationToken.</param>
+	/// <returns>Loaded ItgManiaPack or null if loading failed.</returns>
+	public static async Task<ItgManiaPack> LoadAsync(string filePath, CancellationToken token)
+	{
+		var logger = new ItgManiaPackLogger(filePath);
+		var pack = new ItgManiaPack();
+		try
+		{
+			var iniFile = await IniFile.LoadAsync(filePath, token);
+			var foundGroup = false;
+			foreach (var group in iniFile.Groups)
+			{
+				if (group.Name == TagGroup)
+				{
+					foundGroup = true;
+
+					if (!group.Values.TryGetValue(TagVersion, out var versionString))
+					{
+						throw new Exception($"No {TagVersion} specified.");
+					}
+
+					if (!int.TryParse(versionString, out var version))
+					{
+						throw new Exception($"Malformed {TagVersion} specified: {versionString}. Expected integer.");
+					}
+
+					if (version > LatestVersion)
+					{
+						logger.Warn(
+							$"{TagVersion} is greater than the latest known version {LatestVersion}. Parsing this file as if it is Version {LatestVersion}.");
+					}
+
+					if (version < 1)
+					{
+						logger.Warn($"{TagVersion} is less than 1. Parsing this file as if it is Version {LatestVersion}.");
+					}
+
+					if (!group.Values.TryGetValue(TagTitle, out var title))
+						logger.Warn($"No {TagTitle} specified.");
+					pack.Title = title;
+					if (!group.Values.TryGetValue(TagTitleTransliteration, out var titleTransliteration))
+						logger.Warn($"No {TagTitleTransliteration} specified.");
+					pack.TitleTransliteration = titleTransliteration;
+					if (!group.Values.TryGetValue(TagTitleSort, out var titleSort))
+						logger.Warn($"No {TagTitleSort} specified.");
+					pack.TitleSort = titleSort;
+					if (!group.Values.TryGetValue(TagSeries, out var series))
+						logger.Warn($"No {TagSeries} specified.");
+					pack.Series = series;
+					if (!group.Values.TryGetValue(TagYear, out var yearString))
+					{
+						logger.Warn($"No {TagYear} specified.");
+					}
+					else
+					{
+						if (!int.TryParse(yearString, out var year))
+						{
+							logger.Warn($"Malformed {TagYear} specified: {yearString}. Expected integer.");
+						}
+						else
+						{
+							pack.Year = year;
+						}
+					}
+
+					if (!group.Values.TryGetValue(TagBanner, out var banner))
+						logger.Warn($"No {TagBanner} specified.");
+					pack.Banner = banner;
+					if (!group.Values.TryGetValue(TagSyncOffset, out var syncOffsetString))
+						logger.Warn($"No {TagSyncOffset} specified.");
+					if (!Enum.TryParse(syncOffsetString, out SyncOffSetType syncOffset))
+					{
+						logger.Warn($"Unknown {TagSyncOffset} specified: {syncOffsetString}.");
+					}
+					else
+					{
+						pack.SyncOffset = syncOffset;
+					}
+
+					break;
+				}
+			}
+
+			if (!foundGroup)
+			{
+				throw new Exception("No [Group] specified.");
+			}
+		}
+		catch (Exception e)
+		{
+			logger.Error($"Failed loading file. {e}");
+			return null;
+		}
+
+		return pack;
+	}
+
+	/// <summary>
+	/// Asynchronously save the pack file to disk.
+	/// </summary>
+	/// <param name="filePath">File path to save to.</param>
+	/// <returns>True is saving was successful and false otherwise.</returns>
+	public async Task<bool> SaveAsync(string filePath)
+	{
+		return await Task.Run(() => Save(filePath));
+	}
+
+	/// <summary>
+	/// Synchronously save the pack file to disk.
+	/// </summary>
+	/// <param name="filePath">File path to save to.</param>
+	/// <returns>True is saving was successful and false otherwise.</returns>
+	public bool Save(string filePath)
+	{
+		var logger = new ItgManiaPackLogger(filePath);
+		try
+		{
+			var iniFile = new IniFile();
+			var group = new IniFile.Group(TagGroup);
+			group.Values.Add(TagVersion, LatestVersion.ToString());
+			group.Values.Add(TagTitle, Title);
+			group.Values.Add(TagTitleTransliteration, TitleTransliteration);
+			group.Values.Add(TagTitleSort, TitleSort);
+			group.Values.Add(TagSeries, Series);
+			group.Values.Add(TagYear, Year.ToString());
+			group.Values.Add(TagBanner, Banner);
+			group.Values.Add(TagSyncOffset, SyncOffset.ToString());
+			iniFile.Groups.Add(group);
+			iniFile.Save(filePath);
+		}
+		catch (Exception e)
+		{
+			logger.Error($"Failed saving. {e}");
+			return false;
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// Returns whether this pack should be considered the same as the given pack.
+	/// This is implemented as a custom method rather than use IEquatable because
+	/// this object is expected to be mutable.
+	/// </summary>
+	/// <param name="other">Other ItgManiaPack to compare to.</param>
+	/// <returns>
+	/// True if this pack should be considered the same as the given pack and false otherwise.
+	/// </returns>
+	public bool Matches(ItgManiaPack other)
+	{
+		if (other is null)
+			return false;
+		if (ReferenceEquals(this, other))
+			return true;
+		return Title == other.Title
+		       && TitleTransliteration == other.TitleTransliteration
+		       && TitleSort == other.TitleSort
+		       && Series == other.Series
+		       && Year == other.Year
+		       && Banner == other.Banner
+		       && SyncOffset == other.SyncOffset;
+	}
+
+	#region ICloneable
+
+	/// <summary>
+	/// Clones this ItgManiaPack.
+	/// </summary>
+	/// <returns>Cloned ItgManiaPack.</returns>
+	public object Clone()
+	{
+		return new ItgManiaPack
+		{
+			Title = Title,
+			TitleTransliteration = TitleTransliteration,
+			TitleSort = TitleSort,
+			Series = Series,
+			Year = Year,
+			Banner = Banner,
+			SyncOffset = SyncOffset,
+		};
+	}
+
+	#endregion ICloneable
+}
+
+/// <summary>
+/// Logger to help identify the Pack in the logs.
+/// </summary>
+public class ItgManiaPackLogger : ILogger
+{
+	private readonly string FilePath;
+	private const string Tag = "[ITGmania Pack]";
+
+	public ItgManiaPackLogger(string filePath)
+	{
+		FilePath = filePath;
+	}
+
+	public void Info(string message)
+	{
+		if (!string.IsNullOrEmpty(FilePath))
+			Logger.Info($"{Tag} [{FilePath}] {message}");
+		else
+			Logger.Info($"{Tag} {message}");
+	}
+
+	public void Warn(string message)
+	{
+		if (!string.IsNullOrEmpty(FilePath))
+			Logger.Warn($"{Tag} [{FilePath}] {message}");
+		else
+			Logger.Warn($"{Tag} {message}");
+	}
+
+	public void Error(string message)
+	{
+		if (!string.IsNullOrEmpty(FilePath))
+			Logger.Error($"{Tag} [{FilePath}] {message}");
+		else
+			Logger.Error($"{Tag} {message}");
+	}
+}
