98038d4236e1383aead24d578b092c157232e7bb
Perry Lynch <perryklynch@gmail.com>
2025-12-22 18:07:01 -0500

    Update MainThreadDispatcher to optionally execute actions immediately if they are already enqueued on the main thread.

--- Fumen/MainThreadDispatcher.cs
+++ Fumen/MainThreadDispatcher.cs
@@ -18,6 +18,47 @@ public sealed class MainThreadDispatcher
 	private static readonly ConcurrentQueue<Action> Actions = new();
 
 	/// <summary>
+	/// Main thread id.
+	/// </summary>
+	private static int? MainThreadId;
+
+	/// <summary>
+	/// Whether to run actions immediately if they are being enqueued on the main thread already.
+	/// </summary>
+	private static bool RunMainThreadActionsImmediatelyIfAlreadyOnMainThread;
+
+	/// <summary>
+	/// Sets the main thread id. Necessary if configuring the MainThreadDispatcher to execute
+	/// actions enqueued on the main thread immediately.
+	/// </summary>
+	/// <param name="threadId">The main thread id.</param>
+	public static void SetMainThreadId(int threadId)
+	{
+		MainThreadId = threadId;
+	}
+
+	/// <summary>
+	/// Configures the MainThreadDispatcher to execute enqueued actions immediately if they are
+	/// being enqueued on the main thread already.
+	/// </summary>
+	/// <param name="runImmediately"></param>
+	public static void SetRunMainThreadActionsImmediatelyIfAlreadyOnMainThread(bool runImmediately)
+	{
+		if (MainThreadId == null && runImmediately)
+			throw new InvalidOperationException("SetMainThreadId must be called to before enabling immediate action execution.");
+		RunMainThreadActionsImmediatelyIfAlreadyOnMainThread = runImmediately;
+	}
+
+	/// <summary>
+	/// Returns whether or not the current thread is the main thread.
+	/// </summary>
+	/// <returns>True if the current thread is the main thread and false otherwise.</returns>
+	private static bool IsOnMainThread()
+	{
+		return MainThreadId == Environment.CurrentManagedThreadId;
+	}
+
+	/// <summary>
 	/// Runs a given task and enqueues the given continuation to run later on the main thread.
 	/// </summary>
 	/// <param name="task">Potentially asynchronous task to run.</param>
@@ -25,7 +66,10 @@ public sealed class MainThreadDispatcher
 	public static async Task RunContinuationOnMainThread(Task task, Action continuation)
 	{
 		await task;
-		Actions.Enqueue(continuation);
+		if (RunMainThreadActionsImmediatelyIfAlreadyOnMainThread && IsOnMainThread())
+			continuation();
+		else
+			Actions.Enqueue(continuation);
 	}
 
 	/// <summary>
@@ -37,7 +81,10 @@ public sealed class MainThreadDispatcher
 	public static async Task RunContinuationOnMainThread<T>(Task<T> task, Action<T> continuation)
 	{
 		var t = await task;
-		Actions.Enqueue(() => continuation(t));
+		if (RunMainThreadActionsImmediatelyIfAlreadyOnMainThread && IsOnMainThread())
+			continuation(t);
+		else
+			Actions.Enqueue(() => continuation(t));
 	}
 
 	/// <summary>
@@ -46,7 +93,10 @@ public sealed class MainThreadDispatcher
 	/// <param name="action">Action to run on the main thread.</param>
 	public static void RunOnMainThread(Action action)
 	{
-		Actions.Enqueue(action);
+		if (RunMainThreadActionsImmediatelyIfAlreadyOnMainThread && IsOnMainThread())
+			action();
+		else
+			Actions.Enqueue(action);
 	}
 
 	/// <summary>
